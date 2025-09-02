8f828d11ecbb571db46484a13f95b832468e3102
Micahaha <xicer029@gmail.com>
2025-09-02 10:02:40 -0400

    portapp

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gD5UqwB2dezotSjNzuO2xTRvjKHIO9CY966z0SBFhxA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
