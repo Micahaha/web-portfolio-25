2894a6bf9024776dc5bfdf39ce1d172d00e5e831
Micahaha <xicer029@gmail.com>
2025-09-02 23:22:34 -0400

    portfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+ const nextConfig = {
+      output: 'export', // Enables static HTML export
+      basePath: process.env.NODE_ENV === 'production' ? '/web-web-portfolio-25' : '', // Adjust for GitHub Pages URL
+      assetPrefix: process.env.NODE_ENV === 'production' ? '/web-web-portfolio-25/' : '', // Adjust for GitHub Pages URL
+    };
 
 export default nextConfig
