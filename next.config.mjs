e984a9e95cbc680eaae8a52a26b52d25fb601e22
Micahaha <xicer029@gmail.com>
2025-09-02 23:33:09 -0400

    ghpages

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
- const nextConfig = {
-      output: 'export', // Enables static HTML export
-      basePath: process.env.NODE_ENV === 'production' ? '/web-web-portfolio-25' : '', // Adjust for GitHub Pages URL
-      assetPrefix: process.env.NODE_ENV === 'production' ? '/web-web-portfolio-25/' : '', // Adjust for GitHub Pages URL
-    };
-
-export default nextConfig
